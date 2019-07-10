f1ac8da7d564c20c56dcc88a91bc0c3c9f431cb5
elector-bg <joshua@DESKTOP-QMBQHMJ.localdomain>
2019-07-10 20:58:15 +0300

    .

--- SQL Scripts/Checks.sql
+++ SQL Scripts/Checks.sql
@@ -1,5 +1,5 @@
 SELECT  protocols.'2'
 FROM    protocols
-WHERE   protocols.'5' <> protocols.'9' + protocols.'10' + protocols.'11' +
-        protocols.'12' + protocols.'13' + protocols.'14' + protocols.'14'
+WHERE   protocols.'5' <> protocols.'11' + protocols.'12' + protocols.'13' +
+        protocols.'14' + protocols.'15' + protocols.'16' + protocols.'17'
 ;
