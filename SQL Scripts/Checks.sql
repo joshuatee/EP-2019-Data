8486a18ad949b19ac109cd9fd2b0f10bb1d2016e
elector-bg <joshua@DESKTOP-QMBQHMJ.localdomain>
2019-07-10 20:47:30 +0300

    .

---
+++ SQL Scripts/Checks.sql
@@ -0,0 +1,5 @@
+SELECT  protocols.'2'
+FROM    protocols
+WHERE   protocols.'5' <> protocols.'9' + protocols.'10' + protocols.'11' +
+        protocols.'12' + protocols.'13' + protocols.'14' + protocols.'14'
+;
